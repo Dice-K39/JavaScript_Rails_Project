84d4788f75e2adb55bfe210adb157597cfab5916
Dice-K39 <daisukehashimoto39@gmail.com>
2020-11-28 21:54:02 -0500

    Fixed bug where the search was not searching by state.

--- recreational_activities_backend/app/controllers/search_controller.rb
+++ recreational_activities_backend/app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
     def search
-        url = "https://ridb.recreation.gov/api/v1/facilities?query=#{params[:query]}&limit=#{params[:num_records]}&offset=0&state=#{params[:state]}"
+        url = "https://ridb.recreation.gov/api/v1/facilities?query=#{params[:query]}&limit=#{params[:num_records]}&offset=0&state=#{params[:states]}"
         response = RestClient.get(url, headers = {"apikey" => ENV["API_KEY"]})
         data = JSON.parse(response.body)
         
